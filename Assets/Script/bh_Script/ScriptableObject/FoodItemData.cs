c1c193fc337a0e6e7b52d9bdce2a1ddbaef58d68
gydns9408 <81347876+gydns9408@users.noreply.github.com>
2023-03-30 22:31:53 +0900

    음식 아이템 클래스 생성

---
+++ Assets/Script/bh_Script/ScriptableObject/FoodItemData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AffectStatus
+{
+    Hp = 0
+}
+
+[CreateAssetMenu(menuName = "Scriptable Object/FoodItemData")]
+public class FoodItemData : ItemData
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    protected AffectStatus itemsAffectStatus;
+    public AffectStatus ItemsAffectStatus { get => itemsAffectStatus; }
+}
