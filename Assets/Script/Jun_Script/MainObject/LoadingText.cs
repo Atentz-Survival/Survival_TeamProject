85337aaac03e1b7a25eb752650c84193178a780c
JunheeLee7 <leejunhee4@naver.com>
2023-04-10 08:59:51 +0900

    04/10 로딩 씬 생성완료
    1. 코루틴을 이용한 로딩씬내의 텍스트 변경.
    2. 로딩 씬을 슬라이더를 이용해서 Update에 적용시켜 슬라이더가 차오르는 것을 이용
    ★할것
    -슬라이더가 다찼을 때 씬을 불러올 것.
    - Sunshine스크립트에서 inNight가 true가되고, 텐트의 오브젝트에 f키를 누를경우 로딩 씬이 뜨며 , 로딩이 끝나면 다시 아침으로 리셋시키기

---
+++ Assets/Script/Jun_Script/MainObject/LoadingText.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingText : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text1 = null;
+    [SerializeField] Slider slider = null;
+    [SerializeField] TextMeshProUGUI text2 = null;
+
+    private float nowTime;              // 현재
+    private float startTime;            // 시작
+    private float endTime = 6;          // 끝
+
+    void Start()
+    {
+        nowTime = endTime;
+        startTime = Time.time;
+        ChargeSlider(0);
+        StartCoroutine(ChnageText(1.5f, text1, text2));
+    }
+
+    void Update()
+    {
+        Check_Loading();
+    }
+
+    public IEnumerator ChnageText(float time, TextMeshProUGUI i, TextMeshProUGUI j)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        j.color = new Color(j.color.r, j.color.g, j.color.b, 0);
+
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / time));
+            yield return null;
+        }
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / time));
+            yield return null;
+        }
+
+        while (j.color.a < 1.0f)
+        {
+            j.color = new Color(j.color.r, j.color.g, j.color.b, j.color.a + (Time.deltaTime / time));
+            yield return null;
+        }
+        j.color = new Color(j.color.r, j.color.g, j.color.b, 1);
+    }
+
+    private void Check_Loading()
+    {
+        nowTime = Time.time - startTime;              // 현재 시간 = 이기능의 시작시간 - 첫동작 시간
+        if (nowTime < endTime)                    // 현재시간이 로딩 시간보다 작을경우
+        {
+            ChargeSlider(nowTime / endTime);    // 슬라이더의 게이지 = 현재시간 / 로딩시간
+        }
+        else
+        {
+            FullSlider();                                  // 현재시간이 로딩시간보다 크거나 같을경우
+        }
+    }
+
+    private void FullSlider()
+    {
+        ChargeSlider(1);                                  // 슬라이더의 게이지가 풀
+        // SceneManager.LoadSceneAsync("SampleScene");
+    }
+
+    private void ChargeSlider(float chargeValue)
+    {
+        slider.value = chargeValue;                              // 슬라이더값을 송출
+    }
+
+}
--- Assets/Script/Jun_Script/MainObject/Sunshine.cs
+++ Assets/Script/Jun_Script/MainObject/Sunshine.cs
@@ -10,7 +10,7 @@ public class Sunshine : MonoBehaviour
     public static ReSpawnObject OnRespawn;                 // 이 클래스내의 함수를 OnRespawn변수로 참조
 
 
-public Material skybox;
+    public Material skybox;
     float alpha = 0.0f;
     public float beta = 0.0008f;
 
@@ -30,7 +30,7 @@ public Material skybox;
 
     private void Start()
     {
-        OnRespawn = SunLight;
+        // OnRespawn = SunLight;
         OnRespawn = SunRotate;
         morningFog = RenderSettings.fogDensity;
     }
@@ -38,6 +38,7 @@ public Material skybox;
     private void Update()
     {
         OnRespawn();
+        SunRotate();
     }
 
     public void SunRotate()
@@ -50,28 +51,18 @@ public Material skybox;
         vec = Quaternion.Euler(round, 0, 0);
         transform.rotation = vec;  // 1초에 0.25도 만큼 회전
                                    // Debug.Log(transform.rotation.x);
+        if(round >= 360)
+            {
+            t = 0;
+        }
         // Debug.Log(vec);
-    }
 
-    void SunLight()
-    {
-        if (transform.eulerAngles.x <= 10) //  0 <= x <= 170
+        if ((vec.x >= 0 && vec.x <= 0.0001f) || (vec.x >= -0.0001f && vec.x <= 0)) //  0 <= x <= 170
         {
             isNight = false;                    // 낮이 된다.
-
-            if (alpha < 0.6f && alpha >= 0.0f)      // -0.1~
-            {
-                alpha += beta * forTimeInGame * t;
-                skybox.SetFloat("_CubemapTransition", alpha);   // CubemapTrasition의 수치가 alpha가 된다. : 0 ~ 0.6
-            }
-
-            else if(alpha >= maxAlpha)
-            {
-                alpha = maxAlpha;                               // 0.6
-            }
         }
 
-        else if (transform.eulerAngles.x < 0.0f && transform.eulerAngles.x >= 170)     // Light의 x값이 170보다 커지면
+        else if ((vec.x >=0.9999f && vec.x <= 1.0f)||(vec.x <= -0.9999f && vec.x >= -1.0f))     // Light의 x값이 170보다 커지면
         {
             isNight = true;                     // 밤
         }
@@ -80,18 +71,36 @@ public Material skybox;
 
         if (isNight)                            // 밤이 되면
         {
-            RenderSettings.fogDensity += 0.0001f;
-            if(RenderSettings.fogDensity >= 0.7f)
+            RenderSettings.fogDensity += 0.00002f;              // 여기 조정!!!!!!!!!!!!!!!!!!
+            if (RenderSettings.fogDensity >= 0.6f)              // 여기 조정!!!!!!!!!!!!!!!!!!
             {
-                RenderSettings.fogDensity = 0.7f;
+                RenderSettings.fogDensity = 0.6f;
             }
 
-            skybox.SetFloat("_CubemapTransition", 0.7f);
+            skybox.SetFloat("_CubemapTransition", 0.7f);    
         }
 
         else
         {
-            RenderSettings.fogDensity = morningFog;            // 아침의 fog량은 0.0001로 고정.
+            RenderSettings.fogDensity = morningFog;             // 아침의 fog량은 0.0001로 고정.
+
+            if (alpha < 0.6f && alpha >= 0.0f)      // -0.1~
+            {
+                alpha += beta * forTimeInGame * t;              // 여기 조정!!!!!!!!!!!!!!!!!!
+                skybox.SetFloat("_CubemapTransition", alpha);   // CubemapTrasition의 수치가 alpha가 된다. : 0 ~ 0.6
+            }
+
+            else if (alpha >= maxAlpha)
+            {
+                alpha = maxAlpha;                               // 0.6
+            }
         }
+
     }
+
+    // 텐트와 상호작용시 할 내용
+    // 텐트와 상호작용시 -> 로딩 씬 출현 -> 다시 씬을 불러들여와서 쿼터니언의 범위 값이 0이되게한다.
+    // get set을 이용하면 되지않을까....?
+
+    // 04/10 씬 생성완료 씬을 설정해서 텐트와 상호작용 시 로딩 씬출력 텍스트 필요
 }
