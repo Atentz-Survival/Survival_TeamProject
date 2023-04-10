e4edf4c2ff274bcecd96560be03bd091001abd01
Kimsooyeun <93499760+Kimsooyeun@users.noreply.github.com>
2023-04-10 18:27:07 +0900

    플레이어 툴 교체 1차

--- Assets/Script/ksy_Script/Player/Tool/Pick.cs
+++ Assets/Script/ksy_Script/Player/Tool/Pick.cs
@@ -4,30 +4,23 @@ using UnityEngine;
 
 public class Pick : MonoBehaviour
 {
-    private GameObject[] pick;
-    private int[] pickLevel = { 1, 2, 3 };
-    private string[] pickNames = { "Pick1", "Pick2", "Pick3" };
-
-
-    private void Start()
-    {
-        pick = new GameObject[pickNames.Length];
-        for (int i = 0; i < pickNames.Length; i++)
-        {
-            pick[i] = GameObject.Find(pickNames[i]);
-            pick[i].SetActive(false);
-        }
-    }
-
     public void OnCangePickLevel()
     {
-        if (ItemManager.Instance.itemInventory.GetEquipToolLevel(ToolItemTag.Pickaxe) == pickLevel[ItemManager.Instance.itemInventory.GetEquipToolLevel(ToolItemTag.Pickaxe) - 1])
+        if (ItemManager.Instance.itemInventory.GetEquipToolLevel(ToolItemTag.Pickaxe) > 0)
         {
-            for (int i = 0; i < pickNames.Length; i++)
+            for (int i = 0; i < 3; i++)
             {
-                pick[i].SetActive(false);
+                transform.GetChild(i).gameObject.SetActive(false);
             }
-            pick[ItemManager.Instance.itemInventory.GetEquipToolLevel(ToolItemTag.Pickaxe) - 1].SetActive(true);
+            transform.GetChild(ItemManager.Instance.itemInventory.GetEquipToolLevel(ToolItemTag.Pickaxe) - 1).gameObject.SetActive(true);
+        }
+        else
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+            this.gameObject.SetActive(false);
         }
     }
 }
