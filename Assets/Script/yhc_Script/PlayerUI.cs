a3dd09e8b9c561c7d265da44ca0594b5f5c53b6f
yhc0607 <121930484+yhc0607@users.noreply.github.com>
2023-04-13 15:19:17 +0900

    UI - 퍼즈메뉴 복구

--- Assets/Script/yhc_Script/PlayerUI.cs
+++ Assets/Script/yhc_Script/PlayerUI.cs
@@ -4,40 +4,86 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class PlayerUI : MonoBehaviour
 {
     TextMeshProUGUI HPText;
     Slider HPUI;
     PlayerBase player;
-    public PauseMenu pauseMenu;
+    PauseMenu pauseMenu;
     public Button pauseButton;
-    // r
+    bool IsClosed = true;
+
+    UIAct uiAct;
 
     private void Awake()
     {
-        HPUI = GetComponentInChildren<Slider>();
-        HPText = GetComponentInChildren<TextMeshProUGUI>();
-        pauseButton = GetComponent<Button>();
-
-        pauseButton.onClick.AddListener(CallPauseMenu);
+        uiAct = new UIAct();
+        pauseMenu = transform.Find("PauseMenu").GetComponent<PauseMenu>();
+        int i = 0;
     }
 
     private void Start()
-    { 
+    {
         player = FindObjectOfType<PlayerBase>();
-        pauseMenu = FindObjectOfType<PauseMenu>();
+        pauseButton = GameObject.Find("Pause").GetComponent<Button>();
+        HPUI = GetComponentInChildren<Slider>();
+        HPText = GetComponentInChildren<TextMeshProUGUI>();
+        pauseButton.onClick.AddListener(CallPauseMenu);
     }
-
 
     private void FixedUpdate()
     {
         HPUI.value = player.HP;
         HPText.text = $"{player.HP} / 1000";
     }
+
     private void CallPauseMenu()
     {
-        pauseMenu.gameObject.SetActive(true);
-        Debug.Log("메뉴 불러오기");
+        if(pauseMenu != null)
+        {
+            pauseMenu.gameObject.SetActive(true);
+            IsClosed = false;
+            Time.timeScale = 0;
+        }
+        else if(pauseMenu != null && !IsClosed)
+        {
+            pauseMenu.gameObject.SetActive(true);
+            IsClosed = true;
+        }
+        else
+        {
+            Debug.Log("퍼즈메뉴 오류");
+        }
     }
-}   
+
+    private void OnEnable()
+    {
+        uiAct.UI.Enable();
+        uiAct.UI.Esc.performed += Esc;
+    }
+
+    private void OnDisable()
+    {
+        uiAct.UI.Esc.performed -= Esc;
+        uiAct.UI.Disable();
+    }
+
+    private void Esc(InputAction.CallbackContext _)
+    {
+        if (IsClosed)
+        {
+            pauseMenu.gameObject.SetActive(true);
+            Time.timeScale = 0;
+            IsClosed = false;
+        }
+        else
+        {
+            pauseMenu.gameObject.SetActive(false);
+            Time.timeScale = 1;
+            IsClosed = true;
+        }
+    }
+
+}
