c5b2200c27c74f1e3bb7589d2a91e28e71dc5b0d
Kimsooyeun <93499760+Kimsooyeun@users.noreply.github.com>
2023-04-22 14:42:16 +0900

    제작창 기본기능 완료

--- Assets/Script/ksy_Script/UI/CraftingWindow.cs
+++ Assets/Script/ksy_Script/UI/CraftingWindow.cs
@@ -2,19 +2,43 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class CraftingWindow : MonoBehaviour
 {
     PlayerBase player;
+    Button startCraft;
+
+    public bool canMakeTool = false;
 
     public Action DontAction;
     public Action DoAction;
+
+
+
+    private void Awake()
+    {
+        startCraft = GameObject.Find("startCraft").GetComponent<Button>();
+    }
     private void Start()
     {
         player = FindObjectOfType<PlayerBase>();
         gameObject.SetActive(false);
         player.onMaking += OpenCraftingWindow;
+        startCraft.onClick.AddListener(OnMakeTool);
+    }
+
+    private void FixedUpdate()
+    {
+        startCraft.interactable = canMakeTool;  //불값의 변경에 따른 클릭 가능 여부 계속 체크
+    }
+
+    void OnMakeTool()
+    {
+        if(canMakeTool == true)
+        {
+            Debug.Log("제작 가능");
+        }
     }
 
     private void OpenCraftingWindow()
