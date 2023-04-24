17eb92a3b851992883e4d92b6768ba1dcd14ece7
wooyong-kim <>
2023-04-24 17:30:33 +0900

    04/24 스타트씬뒤의 로딩 추가 , 빌드 수정

--- Assets/Script/ksy_Script/UI/GameStart.cs
+++ Assets/Script/ksy_Script/UI/GameStart.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameStart : MonoBehaviour
-{
-    Button newGame;
-    Button saveLoad;
+//public class GameStart : MonoBehaviour
+//{
+//    Button newGame;
+//    Button saveLoad;
 
-    private void Awake()
-    {
-        newGame = GameObject.Find("NewGame").GetComponent<Button>();
-        saveLoad = GameObject.Find("SaveLoad").GetComponent<Button>();
-    }
+//    private void Awake()
+//    {
+//        newGame = GameObject.Find("NewGame").GetComponent<Button>();
+//        saveLoad = GameObject.Find("SaveLoad").GetComponent<Button>();
+//    }
 
-    private void Start()
-    {
-        newGame.onClick.AddListener(CallDataPanel);
-        saveLoad.onClick.AddListener(CallDataPanel);
-    }
+//    private void Start()
+//    {
+//        newGame.onClick.AddListener(CallDataPanel);
+//        saveLoad.onClick.AddListener(CallDataPanel);
+//    }
 
-    private void CallDataPanel()
-    {
-        SceneManager.LoadScene(2);
-    }
-}
+//    private void CallDataPanel()
+//    {
+//        SceneManager.LoadScene(4);
+//    }
+//}
